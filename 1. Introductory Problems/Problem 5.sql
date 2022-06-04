342a42529fb49ae4ad80d121391e9a27b59dfccd
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-06-04 14:13:19 +0530

    Create Problem 5.sql

---
+++ 1. Introductory Problems/Problem 5.sql
@@ -0,0 +1,6 @@
+-- 5. Orders placed by specific EmployeeID
+-- Show all the orders placed by a specific employee. The EmployeeID for this Employee (Steven Buchanan) is 5.
+
+SELECT	OrderID, OrderDate
+FROM	Orders
+WHERE	EmployeeID = 5;
