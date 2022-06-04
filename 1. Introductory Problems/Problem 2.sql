900f98c07c19052e9c4a7adf970e6fdafdb6a472
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-06-04 14:09:15 +0530

    Create Problem 2.sql

---
+++ 1. Introductory Problems/Problem 2.sql
@@ -0,0 +1,8 @@
+-- 2. Certain fields from Categories
+-- In the Categories table, selecting all the fields using this SQL:
+-- Select * from Categories
+-- …will return 4 columns. We only want to see two
+-- columns, CategoryName and Description.
+
+
+SELECT CategoryName, Description FROM Categories;
