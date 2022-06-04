8da3bcf5a684e30488fb179005b43e7294063237
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-06-04 14:11:57 +0530

    Create Problem 4.sql

---
+++ 1. Introductory Problems/Problem 4.sql
@@ -0,0 +1,8 @@
+-- 4. Sales Representatives in the United States
+-- Now we’d like to see the same columns as above, but
+-- only for those employees that both have the title of Sales
+-- Representative, and also are in the United States.
+
+SELECT	FirstName,LastName,HireDate 
+FROM	Employees
+WHERE	Title = 'Sales Representative' AND Country = 'USA' ;
