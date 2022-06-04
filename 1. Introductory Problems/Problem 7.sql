269e8309b518422f551ae97c47558e0c75df5cba
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-06-04 14:16:46 +0530

    Create Problem 7.sql

---
+++ 1. Introductory Problems/Problem 7.sql
@@ -0,0 +1,8 @@
+7. Products with “queso” in ProductName
+In the products table, we’d like to see the ProductID and
+ProductName for those products where the ProductName
+includes the string “queso”
+
+SELECT	ProductID, ProductName
+FROM	Products
+WHERE	ProductName LIKE '%queso%';
