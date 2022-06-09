f7e9956fe7c78feaea1e819146acc3676847269e
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-06-09 22:47:25 +0530

    Create 23. Problem.sql

---
+++ 2. Intermediate Problems/23. Problem.sql
@@ -0,0 +1,14 @@
+-- Products that need reordering
+
+-- What products do we have in our inventory that should be reordered? For now, just use the fields UnitsInStock and
+-- ReorderLevel, where UnitsInStock is less than the ReorderLevel, ignoring the fields UnitsOnOrder and Discontinued.
+-- Order the results by ProductID.
+
+SELECT		ProductID, 
+			    UnitsInStock, 
+			    ReorderLevel
+FROM		  Products
+WHERE		  UnitsInStock < ReorderLevel
+ORDER BY	ProductID;
+© 2022 GitHub, Inc.
+Terms
