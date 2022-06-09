80d758c3c36c7dee5fe2a28a01017ab446548d98
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-06-09 22:49:24 +0530

    Create 25. Problem.sql

---
+++ 2. Intermediate Problems/25. Problem.sql
@@ -0,0 +1,5 @@
+-- 25. High freight charges
+
+-- Some of the countries we ship to have very high freight charges. We'd like to investigate some more shipping
+-- options for our customers, to be able to offer them lower freight charges. Return the three ship countries with the
+-- highest average freight overall, in descending order by average freight.
