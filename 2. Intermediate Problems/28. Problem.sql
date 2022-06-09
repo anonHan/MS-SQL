bdc761acdc93de6cb63dcb19b6c5c9dc43245286
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-06-09 22:54:22 +0530

    Create 28. Problem.sql

---
+++ 2. Intermediate Problems/28. Problem.sql
@@ -0,0 +1,11 @@
+-- 28. High freight charges - last year
+
+-- We're continuing to work on high freight charges. We now want to get the three ship countries with the highest
+-- average freight charges. But instead of filtering for a particular year, we want to use the last 12 months of
+-- order data, using as the end date the last OrderDate in Orders.
+
+SELECT TOP 3	ShipCountry,AVG(Freight) as AverageFreight
+FROM			Orders	
+WHERE			OrderDate >= DATEADD(year,-1,(SELECT MAX(OrderDate) FROM Orders))
+GROUP BY		ShipCountry
+ORDER BY		AVG(Freight) DESC;
