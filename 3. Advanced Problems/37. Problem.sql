dda5d8d60a5b8b80a812a811d6ccd9dc0e2fb9af
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-06-15 22:38:07 +0530

    Create 37. Problem.sql

---
+++ 3. Advanced Problems/37. Problem.sql
@@ -0,0 +1,9 @@
+--37. Orders - random assortment.
+
+--The Northwind mobile app developers would now like to
+--just get a random assortment of orders for beta testing on
+--their app. Show a random set of 2% of all orders.
+
+SELECT		TOP 2 PERCENT 
+			O.OrderID, O.EmployeeID, O.CustomerID
+FROM		Orders O
