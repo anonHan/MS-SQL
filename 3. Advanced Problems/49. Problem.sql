24550eabd65196d743a25e43b48b09dff39a202d
Sahil Sharma <35657846+anonHan@users.noreply.github.com>
2022-06-18 22:50:56 +0530

    Create 49. Problem.sql

---
+++ 3. Advanced Problems/49. Problem.sql
@@ -0,0 +1,8 @@
+--49. Customer grouping - fix null
+--There's a bug with the answer for the previous question.
+--The CustomerGroup value for one of the rows is null.
+--Fix the SQL so that there are no nulls in the
+--CustomerGroup field.
+
+
+-- There were no NULL values in the above table in my case. And this could be because I am using a slightly different database from the another year.
